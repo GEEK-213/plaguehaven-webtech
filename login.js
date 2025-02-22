40eb527b7dd159773cc464c6a00937e8feab3450
GEEK-213 <faariskhan213@gmail.com>
2025-02-22 20:09:44 +0530

    login

--- login.js
+++ login.js
@@ -4,6 +4,13 @@ const wrapper = document.querySelector('.wrapper');
 signUpBtnLink.addEventListener('click', () => {
     wrapper.classList.toggle('active');
 });
-signInBtnLink.addEventListener('click', () => {
+
+signInBtnLink.addEventListener('click'), () => {
     wrapper.classList.toggle('active');
+document.getElementById('login-button').addEventListener('click', () => {
+    alert('Logging in...');
 });
+
+document.getElementById('signup-button').addEventListener('click', () => {
+    alert('Signing up...');
+})};
